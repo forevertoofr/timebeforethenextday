cb79b2c04307f7b7fccd5ec23d40046d2e2c6e60
haydenledean <52312596+haydenledean@users.noreply.github.com>
2020-02-29 11:59:13 +0100

    First commit
    That the first commit what i need to add more ?

---
+++ README.md
@@ -0,0 +1,8 @@
+: Time before the next day.
+This show the time before the next day.
+
+Credits :
+
+Original code : ajsya
+
+Modified code : haydenledean
---
+++ script.js
@@ -0,0 +1,32 @@
+var audio = new Audio('kahoot.wav');
+audio.loop = true;
+audio.play();
+// Set the date we're counting down to
+  var now = new Date().getTime();
+var countDownDate = new Date(now + 1000 * 60 * 60 * 24).getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = ":O This is a new day !";
+    document.getElementById("message").innerHTML = "Congratulations ! You found a easter egg !";
+  }
+}, 1000);
